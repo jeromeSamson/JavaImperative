8ce9a799941a9773042272546cae510b310cb030
Jérôme SAMSON DIT LAVIGNE <jerome.samson07@gmail.com>
2017-07-04 14:40:17 +0200

    Maj

--- src/fr/operateurs/console/OperateursApp.java
+++ src/fr/operateurs/console/OperateursApp.java
@@ -5,7 +5,7 @@ import java.util.Scanner;
 public class OperateursApp {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		@SuppressWarnings("resource")
 		Scanner scan = new Scanner(System.in);
 		
 		System.out.println("Veuillez saisir le premier nombre ");
